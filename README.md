5080b92ce9b9b6114868a8f5911b128e775075e5
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-05-30 11:20:25 -0500

    Change kas package to kas-container-2.6.2

---
+++ README.md
@@ -0,0 +1,9 @@
+# Packages for `dpkg`
+
+This repository contains metadata for packaging certain tools and libraries under
+`dpkg`, providing installation candidates for Debian-based distributions.
+
+# Building Packages
+
+## Cloning a Source Package
+
--- builder.sh
+++ builder.sh
@@ -9,10 +9,30 @@
 #
 # CREATED:          05/25/2022
 #
-# LAST EDITED:      05/25/2022
+# LAST EDITED:      05/30/2022
 ###
 
 set -e
+
+# If the source resides on GitHub, we have to clone it in a special way.
+get_github_source() {
+    repository=${1%%.git}
+
+    # Get a .tar.gz of the source at the tag
+    printf '%s\n' "Downloading $repository/archive/v$version.tar.gz"
+    curl -L -O $repository/archive/v$version.tar.gz
+
+    # Unzip it into the current directory
+    tar xzvf v$version.tar.gz -C ..
+
+    # Move the source into the parent directory (used by debuild)
+    mv v$version.tar.gz ../${package}_${version}.orig.tar.gz
+}
+
+get_git_source() {
+    >&2 printf '%s\n' "Unimplemented: get_git_source"
+    exit 1
+}
 
 ERROR_MESSAGE="\
 Error. This script must be run from a folder containing a debian/ folder\
@@ -27,14 +47,13 @@ fullname=$(basename $PWD)
 package=${fullname%%-*}
 version=${fullname##*-}
 
-# Get a .tar.gz of the source at the tag
-curl -L -O https://github.com/AmateurECE/$package/archive/v$version.tar.gz
-
-# Unzip it into the current directory
-tar xzvf v$version.tar.gz -C ..
-
-# Move the source into the parent directory (used by debuild)
-mv v$version.tar.gz ../${package}_${version}.orig.tar.gz
+# Get the git repo
+repository_url=$(awk '/^Vcs-Git/{print $2}' debian/control)
+if [[ $repository_url =~ "github.com" ]]; then
+    get_github_source $repository_url
+else
+    get_git_source $repository_url
+fi
 
 # If we just want to download the artifacts, stop here.
 if [[ "1" = ${DOWNLOAD_ONLY:-0} ]]; then
