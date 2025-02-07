71da3206cd079f7a57ddae01942a9dac2971abe5
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-02-07 16:17:16 -0600

    builder: Formatting, set -x

--- builder.sh
+++ builder.sh
@@ -15,29 +15,30 @@
 : ${V_PREFIX:=1}
 
 set -e
+set -x
 
 # If the source resides on GitHub, we have to clone it in a special way.
 get_github_source() {
-    repository=${1%%.git}
-    file=$version.tar.gz
-    if [ "1" = "$V_PREFIX" ]; then
-	    file=v$file
-    fi
+	repository=${1%%.git}
+	file=$version.tar.gz
+	if [ "1" = "$V_PREFIX" ]; then
+		file=v$file
+	fi
 
-    # Get a .tar.gz of the source at the tag
-    printf '%s\n' "Downloading $repository/archive/$file"
-    curl -L -O $repository/archive/$file
+	# Get a .tar.gz of the source at the tag
+	printf '%s\n' "Downloading $repository/archive/$file"
+	curl -L -O $repository/archive/$file
 
-    # Unzip it into the current directory
-    tar xzvf $file -C ..
+	# Unzip it into the current directory
+	tar xzvf $file -C ..
 
-    # Move the source into the parent directory (used by debuild)
-    mv $file ../${package}_${version}.orig.tar.gz
+	# Move the source into the parent directory (used by debuild)
+	mv $file ../${package}_${version}.orig.tar.gz
 }
 
 get_git_source() {
-    >&2 printf '%s\n' "Unimplemented: get_git_source"
-    exit 1
+	printf >&2 '%s\n' "Unimplemented: get_git_source"
+	exit 1
 }
 
 ERROR_MESSAGE="\
@@ -45,8 +46,8 @@ Error. This script must be run from a folder containing a debian/ folder\
 "
 
 if [[ ! -d debian ]]; then
-    >&2 printf '%s\n' "$ERROR_MESSAGE"
-    exit 1
+	printf >&2 '%s\n' "$ERROR_MESSAGE"
+	exit 1
 fi
 
 fullname=$(basename $PWD)
@@ -56,14 +57,14 @@ version=${fullname##*-}
 # Get the git repo
 repository_url=$(awk '/^Vcs-Git/{print $2}' debian/control)
 if [[ $repository_url =~ "github.com" ]]; then
-    get_github_source $repository_url
+	get_github_source $repository_url
 else
-    get_git_source $repository_url
+	get_git_source $repository_url
 fi
 
 # If we just want to download the artifacts, stop here.
 if [[ "1" = ${DOWNLOAD_ONLY:-0} ]]; then
-    exit 0
+	exit 0
 fi
 
 # Build the package
